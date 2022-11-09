71749c53ab68955e61fa8e676de4e66f17198814
hwch0 <hyewon0710@gmail.com>
2022-11-09 19:25:09 +0900

    - 커밋 테스트

--- 999_react_project/src/controller/postController.java
+++ 999_react_project/src/controller/postController.java
@@ -18,7 +18,7 @@ public class postController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		
-		
+		System.out.println("커밋 테스트");
 		
 		
 		try {
